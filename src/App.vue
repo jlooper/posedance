75424333b4804f1dec5e9d70395b3c8c8bf608ab
Jen Looper <jen.looper@gmail.com>
2020-04-17 20:15:59 -0400

    footer

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,17 @@
   <div id="app">
     <Header />
     <router-view />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+
 export default {
   name: "App",
-  components: { Header }
+  components: { Header, Footer }
 };
 </script>
 <style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="columns">
+  <div class="columns foot">
     <div class="column is-full has-background-info">
-      <h1 class="has-text-centered is-size-1 has-text-white">@copy; 2020 Ladeez First Media</h1>
+      <h1 class="has-text-centered is-size-3 has-text-white">&copy; 2020 Ladeez First Media</h1>
     </div>
   </div>
 </template>
+<style scoped>
+.foot {
+  padding-top: 20px;
+}
+</style>
